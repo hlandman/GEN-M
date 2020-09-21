fe64e129da47e2c4eb36f908ab60d3af4170fb6b
Hillel Landman <hlandman@gmail.com>
2020-09-21 14:48:15 -0400

    Added midify_results.

---
+++ midify_results.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from pypianoroll import Multitrack
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Please include experiment directory path.")
+        return 1
+    elif not os.path.exists(sys.argv[1]):
+        print("Experiment path does not exist.")
+        return 1
+    else:
+        exp_path = sys.argv[1]
+
+    # for t in ['fake_x_bernoulli_sampling', 'fake_x_hard_thresholding']:
+    #     for i in range(10):
+    #         result_path = os.path.join(exp_path, 'results/inference/pianorolls/{}/{}_{}.npz'.format(t, t, i))
+    #         m = Multitrack(result_path)	
+    #         m.write(os.path.join(exp_path, 'results/inference/pianorolls/{}/result_{}.mid'.format(t, i)))
+    #         print(t, i)
+
+    method = 'fake_x_hard_thresholding'
+    for i in range(10):
+        result_path = os.path.join(exp_path, 'results/inference/pianorolls/{}/{}_{}.npz'.format(method, method, i))
+        m = Multitrack(result_path)	
+        m.write(os.path.join(exp_path, 'results/inference/pianorolls/{}/result_{}.mid'.format(method, i)))
+        print(t, i)
+
+if __name__ == "__main__":
+    main()
